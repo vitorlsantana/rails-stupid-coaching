789dcf3ce557e17dd39fcbfc4f8fbbc7327a09f5
Vitor Santana <santana.vitor@gmail.com>
2020-09-15 16:37:06 -0300

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get '/ask', to: 'questions#ask'
+
+get '/answer', to: 'questions#answer'
+
+root to: 'questions#ask'
+
 end
